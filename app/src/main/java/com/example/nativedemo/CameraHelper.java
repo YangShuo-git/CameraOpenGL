761491b61f712aead27c26846005f4293ab8370a
baiyang <17801003317@163.com>
2025-10-14 23:25:38 +0800

    测试滤镜

--- app/src/main/java/com/example/nativedemo/CameraHelper.java
+++ app/src/main/java/com/example/nativedemo/CameraHelper.java
@@ -11,7 +11,8 @@ import androidx.lifecycle.LifecycleOwner;
 // 该类封装了对Camera的一系列操作
 public class CameraHelper {
     private HandlerThread handlerThread;
-    private CameraX.LensFacing currentFacing = CameraX.LensFacing.BACK;
+    private CameraX.LensFacing backFacing = CameraX.LensFacing.BACK;
+    private CameraX.LensFacing frontFacing = CameraX.LensFacing.FRONT;
     private Preview.OnPreviewOutputUpdateListener listener;
 
 
@@ -25,11 +26,11 @@ public class CameraHelper {
     }
     private Preview getPreView() {
         // 分辨率并不是最终的分辨率，CameraX会自动根据设备的支持情况，结合给的参数，设置一个最为接近的分辨率
+        // 得到camera的数据
         PreviewConfig previewConfig = new PreviewConfig.Builder()
                 .setTargetResolution(new Size(640, 480))
-                .setLensFacing(currentFacing) //前置或者后置摄像头
+                .setLensFacing(frontFacing) //前置或者后置摄像头
                 .build();
-        // 要不  得到他的数据
 
         Preview preview = new Preview(previewConfig);
         preview.setOnPreviewOutputUpdateListener(listener);
--- app/src/main/res/raw/camera_frag.frag
+++ app/src/main/res/raw/camera_frag.frag
@@ -11,14 +11,14 @@ void main(){
      vec4 rgba = texture2D(vTexture, aCoord);
      gl_FragColor=vec4(rgba.r, rgba.g, rgba.b, rgba.a);
 
-     // 1.滤镜测试
-     // vec4 rgba = texture2D(vTexture,aCoord);
-     // float color=(rgba.r+rgba.g+rgba.b) / 3.0;
-     // gl_FragColor=vec4(color,color,color,1);
-     // 2.滤镜测试
-     // vec4 rgba = texture2D(vTexture,aCoord);
-     // gl_FragColor=rgba+vec4(0.3,0.3,0.0,0.0);
+     // 1.黑白滤镜
+//      vec4 rgba = texture2D(vTexture,aCoord);
+//      float color=(rgba.r+rgba.g+rgba.b) / 3.0;
+//      gl_FragColor=vec4(color,color,color,1);
+     // 2.电影滤镜
+//      vec4 rgba = texture2D(vTexture,aCoord);
+//      gl_FragColor=rgba+vec4(0.3,0.3,0.0,0.0);
      // 3.逆时针旋转90°
-     // vec4 rgba = texture2D(vTexture,vec2(1.0-aCoord.y,aCoord.x));
-     // gl_FragColor=vec4(rgba.r,rgba.g,rgba.b,rgba.a);
+//      vec4 rgba = texture2D(vTexture,vec2(1.0-aCoord.y,aCoord.x));
+//      gl_FragColor=vec4(rgba.r,rgba.g,rgba.b,rgba.a);
 }
